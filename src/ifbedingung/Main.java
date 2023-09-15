b8bcb14615208cba03cde7f26c202bebf98336ae
Damien Wallis <damie.grobi@gmail.com>
2023-09-15 09:03:47 +0200

    finished If-Bedingung-Aufgabe 1

---
+++ src/ifbedingung/Main.java
@@ -0,0 +1,30 @@
+package ifbedingung;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Erstelle eine Zufallszahl zwischen 0 und 100
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        // Gib die Zufallszahl aus
+        System.out.print(randomNumber);
+
+        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+        if (randomNumber < 20) {
+            System.out.print(" Mini");
+        }
+
+        // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium
+        else if (randomNumber > 20 && randomNumber < 50) {
+            System.out.print(" Medium");
+        }
+
+        // Wenn die Zahl größer als 50 ist gib aus "Large
+        else if (randomNumber > 50) {
+            System.out.print(" Large");
+        }
+    }
+}
