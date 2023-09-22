9c395b927316ae1aa66d823f0e1b7ebc7daf1b66
Damien Wallis <damie.grobi@gmail.com>
2023-09-22 08:20:42 +0200

    finished loops1

---
+++ src/at/dam/units/loops1.java
@@ -0,0 +1,11 @@
+package at.dam.units;
+
+public class loops1 {
+    public static void main(String[] args) {
+        int summe = 0;
+        for (int i = 0; i < 101; i++) {
+            summe = summe + i;
+        }
+        System.out.println(summe);
+    }
+}
