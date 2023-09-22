2c00c831492d9b713f42eb3db0eecbe908b694ad
Damien Wallis <damie.grobi@gmail.com>
2023-09-22 08:59:35 +0200

    finished whilee

---
+++ src/at/dam/units/whilee.java
@@ -0,0 +1,22 @@
+package at.dam.units;
+
+import java.util.Random;
+
+public class whilee {
+    public static void main(String[] args) {
+        Random random = new Random();
+        boolean isFinished = false;
+
+        int sum = 0;
+        while (!isFinished) {
+            int randomNumber = random.nextInt(10, 30);
+            sum = sum + randomNumber;
+
+            if (randomNumber == 15 || randomNumber == 25) {
+                isFinished = true;
+            }
+
+        }
+        System.out.println(sum);
+    }
+}
