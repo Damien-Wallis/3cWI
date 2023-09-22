1997fa3348201a5d26347d53140bc07c1ee41688
Damien Wallis <damie.grobi@gmail.com>
2023-09-22 08:27:52 +0200

    changed summe to sum

--- src/at/dam/units/loops2.java
+++ src/at/dam/units/loops2.java
@@ -2,10 +2,10 @@ package at.dam.units;
 
 public class loops2 {
     public static void main(String[] args) {
-        int summe = 0;
+        int sum = 0;
         for (int i = 2; i < 1001; i = i+2) {
-            summe = summe + i;
+            sum = sum + i;
         }
-        System.out.println(summe);
+        System.out.println(sum);
     }
 }
