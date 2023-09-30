4b3e29773be7d18cee4f669e34d8e3f4bb04f47b
Damien Wallis <damie.grobi@gmail.com>
2023-09-30 10:17:12 +0200

    finished bankomat

---
+++ src/at/dam/units/bankomat.java
@@ -0,0 +1,53 @@
+package at.dam.units;
+
+import java.util.Scanner;
+
+public class bankomat {
+    public static void main(String[] args) {
+
+        boolean isFinished = false;
+        Scanner scanner = new Scanner(System.in);
+        int balance = 0;
+
+        System.out.println("Selektieren Sie die gewünschte Funktion:");
+        System.out.println("    1. Einzahlen");
+        System.out.println("    2. Abheben");
+        System.out.println("    3. Kontostand");
+        System.out.println("    4. Ende");
+
+        while (!isFinished) {
+
+            int selection = scanner.nextInt();
+
+            //Einzahlen
+            if (selection == 1) {
+                System.out.println("Geben Sie den Betrag ein, den Sie einzahlen möchten: ");
+                selection = scanner.nextInt();
+
+                balance = balance + selection;
+                System.out.println("Sie haben " + selection + " Euro eingezahlt!");
+            }
+
+            //Abheben
+            else if (selection == 2) {
+                System.out.println("Geben Sie den Betrag ein, den Sie abheben möchten");
+                selection = scanner.nextInt();
+
+                balance = balance - selection;
+                System.out.println("Sie haben " + selection + " Euro abgehoben!");
+            }
+
+            //Kontostand
+            else if (selection == 3) {
+                System.out.println("Der Kontostand beträgt: " + balance + " Euro!");
+            }
+
+            //Beenden
+            else if (selection == 4) {
+                isFinished = true;
+                System.out.println("Maschine gestoppt");
+            }
+        }
+        scanner.close();
+    }
+}
