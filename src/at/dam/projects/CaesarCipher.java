e6d4910533e0b19b2e6d0c0006b22675b7d44888
Damien Wallis <damie.grobi@gmail.com>
2023-10-18 08:45:37 +0200

    added Array, Scanner and tried to make an encrypt function

--- src/at/dam/projects/CaesarCipher.java
+++ src/at/dam/projects/CaesarCipher.java
@@ -6,9 +6,15 @@ public class CaesarCipher {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String[] alphabet = {"a", "b", "c", "d", "e", "f", "g","h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"};
+        String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
-        System.out.println("Welchen Text möchten Sie verschlüsseln?");
+        System.out.println("Welchen Text moechten Sie verschlüsseln?");
         String text = scanner.nextLine();
+
+        String encryptedText = encrypt;
+    }
+    public static String encrypt(String string, int ){
+
+        System.out.println("Verschlüsseltes Wort: " + encryptedText);
     }
 }
