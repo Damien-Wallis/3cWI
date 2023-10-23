1c5f2488cd23072351194cfac9ae08b4d78a916c
Damien Wallis <damie.grobi@gmail.com>
2023-10-23 14:17:27 +0200

    finished caesar cipher

--- src/at/dam/projects/CaesarCipher.java
+++ src/at/dam/projects/CaesarCipher.java
@@ -6,15 +6,38 @@ public class CaesarCipher {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-
+        int offset = 2;
         System.out.println("Welchen Text moechten Sie verschlüsseln?");
         String text = scanner.nextLine();
+        System.out.println("Zu verschlüsselnder Text: " + text);
 
-        String encryptedText = encrypt;
+        String encryptedText = CaesarCipher.encrypt(text, offset);
+        System.out.println("Verschlüsselter Text: " + encryptedText);
+
+        String decryptedText = CaesarCipher.decrypt(encryptedText, offset);
+        System.out.println("Entschlüsselter Text: " + decryptedText);
     }
-    public static String encrypt(String string, int ){
 
-        System.out.println("Verschlüsseltes Wort: " + encryptedText);
+    //Encrypt Funktion
+    public static String encrypt(String text, int offset) {
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            char encryptedC = (char) (c + offset);
+            encryptedText.append(encryptedC);
+        }
+        return encryptedText.toString();
+    }
+
+    public static String decrypt(String text, int offset) {
+        StringBuilder decryptedText = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            char decryptedC = (char) (c - offset);
+            decryptedText.append(decryptedC);
+        }
+        return decryptedText.toString();
     }
 }
