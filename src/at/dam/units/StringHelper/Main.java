874c945d3fe672c33b22a6beedac4ffc3e99dd61
Damien Wallis <damie.grobi@gmail.com>
2023-10-25 08:55:53 +0200

    made separate files for the functions and finished Main

---
+++ src/at/dam/units/StringHelper/Main.java
@@ -0,0 +1,23 @@
+package at.dam.units.StringHelper;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //isPalindrome
+        String isPalindrome = Palindrome.palindrome;
+        System.out.println("Ist der Text ein Palindrom? " + isPalindrome);
+
+        //countLetters
+        String countLetters = CountLetters.countLetters;
+        System.out.println("Anzahl der Buchstaben im Text " + countLetters);
+
+        //reverse String
+        String reverseString = ReverseString.reverseString;
+        System.out.println("Der Text rückwärts geschrieben lautet: " + reverseString);
+
+        //printAmountOfLetters
+        String letterAmount = LetterAmount.letterAmount;
+        System.out.println("Anzahl der einzelnen Buchstaben im Text: " + letterAmount);
+
+    }
+}
