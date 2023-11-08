d8204cdc980a63ccf0261219d38c03be996a27a6
Damien Wallis <damie.grobi@gmail.com>
2023-11-08 09:10:44 +0100

    corrected Palindrome function and implemented an user input

--- src/at/dam/units/StringHelper/Main.java
+++ src/at/dam/units/StringHelper/Main.java
@@ -1,16 +1,21 @@
 package at.dam.units.StringHelper;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String text = "uhu";
+        System.out.println("Geben Sie einen beliebigen Text ein: ");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
         //isPalindrome
-        Boolean isPalindrome = StringHelper.palindrome(text);
+        Boolean isPalindrome = StringHelper.isPalindrome(text);
         System.out.println("Ist der Text ein Palindrom? " + isPalindrome);
 
+        /*
         //countLetters
         int countLetters = StringHelper.countLetters(text);
         System.out.println("Anzahl der Buchstaben im Text " + countLetters);
-        /*
+
         //reverse String
         String reverseString = StringHelper.reverseString;
         System.out.println("Der Text rückwärts geschrieben lautet: " + reverseString);
--- src/at/dam/units/StringHelper/StringHelper.java
+++ src/at/dam/units/StringHelper/StringHelper.java
@@ -1,20 +1,24 @@
 package at.dam.units.StringHelper;
 
 public class StringHelper {
-    public static Boolean palindrome(String text) {
+    public static Boolean isPalindrome(String text) {
         for (int i = 0; i < text.length() / 2; i++) {
-            if (text.charAt(i) == text.charAt(text.length() - i - 1)) {
-                return true;
+            if (text.charAt(i) != text.charAt(text.length() - i - 1)) {
+                return false;
             }
 
         }
-        return false;
+        return true;
     }
 
-    public static int countLetters(String text, char character) {
-
-
-    }return character;
+    /*public static int countLetters(String text, char character) {
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            
+        }
+        return count;
+    }
+     */
 }
 
 
