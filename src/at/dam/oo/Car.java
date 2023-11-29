88bb3948fea437607e6da9aac25598d577dc45b0
Damien Wallis <damie.grobi@gmail.com>
2023-11-29 09:00:45 +0100

    added method "turboBoost"

--- src/at/dam/oo/Car.java
+++ src/at/dam/oo/Car.java
@@ -9,15 +9,26 @@ public class Car {
     public String brand;
     public String serialNumber;
     public String color;
+    public int tankVolume;
 
     //Methode drive
     public void drive() {
-        this.fuelAmount = this.fuelAmount - fuelConsumption;
+        this.fuelAmount = this.fuelAmount - this.fuelConsumption;
         System.out.println("I am driving");
     }
 
     //Methode brake
     public void brake(){
         System.out.println("I am braking");
+    }
+
+    //Methode turboBoost
+    public void turboBoost(){
+        if (fuelAmount > this.tankVolume * 0.1){
+            System.out.println("SuperBoostMode");
+        }
+        else {
+            System.out.println("Not enough fuel to go to SuperBoostMode");
+        }
     }
 }
--- src/at/dam/oo/Main.java
+++ src/at/dam/oo/Main.java
@@ -2,15 +2,15 @@ package at.dam.oo;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 7;
 
         //Auto 1
         Car c1 = new Car();
         c1.brand = "Porsche";
         c1.fuelConsumption = 9;
         c1.serialNumber = "P911";
-        c1.fuelAmount = 20;
+        c1.fuelAmount = 10;
         c1.color = "cyan";
+        c1.tankVolume = 150;
 
         //Auto 2
         Car c2 = new Car();
@@ -19,6 +19,7 @@ public class Main {
         c2.serialNumber = "H123";
         c2.fuelAmount = 70;
         c2.color = "yellow";
+        c2.tankVolume = 80;
 
         //Methode drive
         System.out.println("Current fuel amount is " + c2.fuelAmount);
@@ -27,5 +28,11 @@ public class Main {
 
         //Methode brake
         c1.brake();
+
+        //Methode TurboBoostMode
+        System.out.print("Car 1: ");
+        c1.turboBoost();
+        System.out.print("Car 2: ");
+        c2.turboBoost();
     }
 }
