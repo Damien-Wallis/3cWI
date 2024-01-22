652f823f056672c5a947deed5a87dd5f8482684a
Damien Wallis <damie.grobi@gmail.com>
2024-01-22 13:48:22 +0100

    added chargingStatus to Battery and created a constructor for Remote

--- src/at/dam/oo/remote/Battery.java
+++ src/at/dam/oo/remote/Battery.java
@@ -1,4 +1,17 @@
 package at.dam.oo.remote;
 
 public class Battery {
+    private int chargingStatus;
+
+    public Battery(int chargingStatus) {
+        this.chargingStatus = chargingStatus;
+    }
+
+    public int getChargingStatus() {
+        return chargingStatus;
+    }
+
+    public void setChargingStatus(int chargingStatus) {
+        this.chargingStatus = chargingStatus;
+    }
 }
--- src/at/dam/oo/remote/Remote.java
+++ src/at/dam/oo/remote/Remote.java
@@ -1,4 +1,13 @@
 package at.dam.oo.remote;
 
 public class Remote {
+    private boolean hasPower;
+    private boolean isOn;
+    private Battery battery;
+
+    public Remote(boolean hasPower, boolean isOn, Battery battery) {
+        this.hasPower = hasPower;
+        this.isOn = isOn;
+        this.battery = battery;
+    }
 }
