67043a4c9726947f39e6e06d50219ee4d3afe037
Damien Wallis <damie.grobi@gmail.com>
2024-01-22 14:39:56 +0100

    added addBattery method

--- src/at/dam/oo/remote/Remote.java
+++ src/at/dam/oo/remote/Remote.java
@@ -1,13 +1,21 @@
 package at.dam.oo.remote;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Remote {
     private boolean hasPower;
     private boolean isOn;
-    private Battery battery;
+    private List<Battery> batteries;
 
-    public Remote(boolean hasPower, boolean isOn, Battery battery) {
+    public Remote(boolean hasPower, boolean isOn) {
         this.hasPower = hasPower;
         this.isOn = isOn;
-        this.battery = battery;
+        this.batteries = new ArrayList<>();
+    }
+
+    //Methode addBattery
+    public void addBattery(Battery battery) {
+        this.batteries.add(battery);
     }
 }
