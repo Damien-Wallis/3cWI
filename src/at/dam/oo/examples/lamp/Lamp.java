de2bfe2a1366770c9e940ae771c9e3e5659ef4c4
Damien Wallis <damie.grobi@gmail.com>
2024-02-19 14:13:29 +0100

    added constructor, getter and setter to Lamp

--- src/at/dam/oo/examples/lamp/Lamp.java
+++ src/at/dam/oo/examples/lamp/Lamp.java
@@ -1,4 +1,29 @@
 package at.dam.oo.examples.lamp;
 
+import java.util.List;
+
 public class Lamp {
+    private String name;
+    private List<GlowElement> glowElements;
+
+    public Lamp(String name, List<GlowElement> glowElements) {
+        this.name = name;
+        this.glowElements = glowElements;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<GlowElement> getGlowElements() {
+        return glowElements;
+    }
+
+    public void setGlowElements(List<GlowElement> glowElements) {
+        this.glowElements = glowElements;
+    }
 }
