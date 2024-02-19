8ef8c7c952616b6d3fdd2bb7e4004b017d792f70
Damien Wallis <damie.grobi@gmail.com>
2024-02-19 14:01:42 +0100

    added constructor, getter and setter to GlowElement

--- src/at/dam/oo/examples/lamp/GlowElement.java
+++ src/at/dam/oo/examples/lamp/GlowElement.java
@@ -1,4 +1,37 @@
 package at.dam.oo.examples.lamp;
 
 public class GlowElement {
+    private String color;
+    private int powerConsumption;
+    private boolean isOn;
+
+    public GlowElement(String color, int powerConsumption, boolean isOn) {
+        this.color = color;
+        this.powerConsumption = powerConsumption;
+        this.isOn = isOn;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPowerConsumption() {
+        return powerConsumption;
+    }
+
+    public void setPowerConsumption(int powerConsumption) {
+        this.powerConsumption = powerConsumption;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public void setOn(boolean on) {
+        isOn = on;
+    }
 }
