f9b99ec709bc2fc43373ad195948bd12bfbcc55f
Damien Wallis <damie.grobi@gmail.com>
2024-02-28 09:03:53 +0100

    finished BaseCalculator with add, subtract, multiply and divide methods

--- src/at/dam/oo/calculator/BaseCalculator.java
+++ src/at/dam/oo/calculator/BaseCalculator.java
@@ -1,4 +1,29 @@
 package at.dam.oo.calculator;
 
 public class BaseCalculator {
+
+    public double add(double a, double b) {
+        System.out.print("Summe: ");
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        System.out.print("Differenz: ");
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        System.out.print("Produkt: ");
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            System.out.print("Division durch 0 nicht möglich");
+            return 0;
+        } else {
+            System.out.print("Quotient: ");
+            return a / b;
+        }
+    }
 }
--- src/at/dam/oo/calculator/Main.java
+++ src/at/dam/oo/calculator/Main.java
@@ -1,4 +1,11 @@
 package at.dam.oo.calculator;
 
 public class Main {
+    public static void main(String[] args) {
+        BaseCalculator baseCalculator = new BaseCalculator();
+        System.out.println(baseCalculator.add(3,5));
+        System.out.println(baseCalculator.subtract(2,3));
+        System.out.println(baseCalculator.multiply(8,7));
+        System.out.println(baseCalculator.divide(2,3));
+    }
 }
