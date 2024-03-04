0ad3af829b82844e08c683e58bb83a6a2b1766dc
Damien Wallis <damie.grobi@gmail.com>
2024-03-04 13:34:33 +0100

    finished RootCalculator with sinus and cosinus method

--- src/at/dam/oo/calculator/Main.java
+++ src/at/dam/oo/calculator/Main.java
@@ -4,16 +4,24 @@ public class Main {
     public static void main(String[] args) {
         BaseCalculator baseCalculator = new BaseCalculator();
         RootCalculator rootCalculator = new RootCalculator();
+        ScienceCalculator scienceCalculator = new ScienceCalculator();
 
         //BaseCalculater-Funktionen
-        System.out.println(baseCalculator.add(3,5));
-        System.out.println(baseCalculator.subtract(2,3));
-        System.out.println(baseCalculator.multiply(8,7));
-        System.out.println(baseCalculator.divide(2,3));
+        System.out.println(baseCalculator.add(3, 5));
+        System.out.println(baseCalculator.subtract(2, 3));
+        System.out.println(baseCalculator.multiply(8, 7));
+        System.out.println(baseCalculator.divide(2, 3));
         System.out.println(" ");
 
         //RootCalculator-Funktionen
         System.out.println(rootCalculator.sqrrt(4));
-        System.out.println(rootCalculator.add(2,4));
+        System.out.println(rootCalculator.add(2, 4));
+        System.out.println(" ");
+
+        //ScienceCalculator-Funktionen
+        System.out.println(scienceCalculator.sinus(360));
+        System.out.println(scienceCalculator.cosinus(360));
+        System.out.println(scienceCalculator.sqrrt(9));
+        System.out.println(scienceCalculator.add(5,9));
     }
 }
--- src/at/dam/oo/calculator/ScienceCalculator.java
+++ src/at/dam/oo/calculator/ScienceCalculator.java
@@ -1,4 +1,19 @@
 package at.dam.oo.calculator;
 
-public class ScienceCalculator {
+public class ScienceCalculator extends RootCalculator {
+    public double sinus(double a){
+        double sum = 0;
+        if (a >= -360 && a <= 360){
+            sum = Math.sin(a);
+            System.out.print("Sinus von " + a + " = ");
+        }return sum;
+    }
+
+    public double cosinus(double a){
+        double sum = 0;
+        if (a >= -360 && a <= 360){
+            sum = Math.cos(a);
+            System.out.print("Cosinus von " + a + " = ");
+        }return sum;
+    }
 }
