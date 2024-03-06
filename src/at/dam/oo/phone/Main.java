581478c36c7e2284e2ed98cddd86451914cb0114
Damien Wallis <damie.grobi@gmail.com>
2024-03-06 08:54:56 +0100

    PhoneFile: added getInfo method

--- src/at/dam/oo/phone/Main.java
+++ src/at/dam/oo/phone/Main.java
@@ -2,6 +2,7 @@ package at.dam.oo.phone;
 
 public class Main {
     public static void main(String[] args) {
-
+        PhoneFile file = new PhoneFile("jpg", 4, "schule");
+        file.getInfo();
     }
 }
--- src/at/dam/oo/phone/PhoneFile.java
+++ src/at/dam/oo/phone/PhoneFile.java
@@ -6,8 +6,11 @@ public class PhoneFile {
     private String name;
 
     public PhoneFile(String extension, int size, String name) {
-        this.extension = extension;
+        this.extension = "." + extension;
         this.size = size;
         this.name = name;
+    }
+    public void getInfo(){
+        System.out.println(name + extension + ": " + size + "MB");
     }
 }
