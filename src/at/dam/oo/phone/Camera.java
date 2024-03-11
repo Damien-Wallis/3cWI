3b0503c32963148f8a16f78598eee67ea3de894c
Damien Wallis <damie.grobi@gmail.com>
2024-03-11 14:19:41 +0100

    added takePicture method

--- src/at/dam/oo/phone/Camera.java
+++ src/at/dam/oo/phone/Camera.java
@@ -2,8 +2,15 @@ package at.dam.oo.phone;
 
 public class Camera {
     private int resolution;
+    SDCard sdCard;
 
-    public Camera(int resolution) {
+    public Camera(int resolution, SDCard sdCard) {
         this.resolution = resolution;
+        this.sdCard = sdCard;
+    }
+
+    public void takePicture(String extension, String name) {
+        PhoneFile file2 = new PhoneFile(extension, resolution * resolution, name);
+        sdCard.saveFile(file2);
     }
 }
--- src/at/dam/oo/phone/Main.java
+++ src/at/dam/oo/phone/Main.java
@@ -5,10 +5,12 @@ public class Main {
         //Objekte
         PhoneFile file = new PhoneFile("jpg", 4, "schule");
         SDCard card = new SDCard(30);
+        Camera camera = new Camera(20, card);
 
         //Befehle
         System.out.println(file.getInfo(file));
         card.saveFile(file);
+        camera.takePicture("png", "drache");
         card.getAllFiles();
         System.out.println(card.getFreeSpace());
     }
