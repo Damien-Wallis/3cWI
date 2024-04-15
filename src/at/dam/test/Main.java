259be30c243662ad126eb98d691d208b60b210cd
Damien Wallis <damie.grobi@gmail.com>
2024-04-15 13:58:43 +0200

    added informPassengers method

--- src/at/dam/test/Main.java
+++ src/at/dam/test/Main.java
@@ -4,8 +4,18 @@ public class Main {
     public static void main(String[] args) {
         //Objekte
         Passenger p1 = new Passenger("Damien", "Wallis");
+        Passenger p2 = new Passenger("Dragan", "Nikolic");
+
+        Position vienna = new Position(200, 178);
+        Position melbourne = new Position(89, 109);
+
+        Plane pl1 = new Plane("Airbus 1", vienna);
 
         //Befehle
         p1.inform("Your food will be brought to you in 5 min.");
+
+        pl1.addPassengers(p1);
+        pl1.addPassengers(p2);
+        pl1.informPassengers("The flight will be delayed by 30 min.");
     }
 }
--- src/at/dam/test/Plane.java
+++ src/at/dam/test/Plane.java
@@ -1,4 +1,31 @@
 package at.dam.test;
 
+import at.dam.oo.car.RearMirror;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Plane {
+    private String name;
+    private Position position;
+    private List<Passenger> passengers;
+
+    public Plane(String name, Position position) {
+        this.name = name;
+        this.position = position;
+        this.passengers = new ArrayList<>();
+    }
+
+    //Methode addPassengers
+    public void addPassengers(Passenger passenger) {
+        this.passengers.add(passenger);
+    }
+
+    //Methode informPassengers
+    public String informPassengers(String message){
+        for (Passenger passenger:this.passengers) {
+            System.out.println(message);
+        }
+        return message;
+    }
 }
